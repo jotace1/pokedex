ff1bb1307ee18665417d96e6b5e2e88de3540bc2
João <joaocsc01@hotmail.com>
2020-10-07 17:42:51 -0300

    finished

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -93,12 +93,8 @@ export const Main = styled.div`
     display: grid;
     justify-content:  center;
     align-content: center;
-    grid-template-columns: 350px 350px 350px 350px ;
+    grid-template-columns: repeat(auto-fit, minmax(225px, 350px));
     grid-gap: 20px;
-
-    @media( max-width: 1300px){
-        grid-template-columns: 250px 250px 250px 250px;
-    }
 
 
     @media(max-width: 400px){
