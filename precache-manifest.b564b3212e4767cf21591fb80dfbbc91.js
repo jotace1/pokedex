e316b82e153dd2366156f5930565fb6d2dd8d1f4
João <joaocsc01@hotmail.com>
2020-10-07 00:30:12 -0300

    Updates

---
+++ precache-manifest.b564b3212e4767cf21591fb80dfbbc91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3853ff09f15ef041151eb46f12f4f213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bf0503211abf063618d",
+    "url": "/static/js/2.63965d7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63965d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a05abd6e6697aa39055",
+    "url": "/static/js/main.19b1bebd.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "6f1fd44625c52ccd693ca4e39b23cc9f",
+    "url": "/static/media/Pokédex_logo.6f1fd446.png"
+  }
+]);
